c0d8564a14c32c23bf3acc4ba0c457493fc1d623
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-02-14 16:43:06 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#github_version=$(cat github_version.txt)
-#ftp_version=$(cat ftp_version.txt)
-github_version=2020-01-25T03-02-19Z
-ftp_version=2019-07-11T19-31-28Z 
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 
 if [ "$github_version" != "$ftp_version" ]
@@ -18,8 +16,8 @@ then
     if [[ "$github_version" > "$ftp_version" ]]
     then
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minio-mc/latest mc-$github_version"
-        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minio-mc/latest/mc-$ftp_version" 
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minio-mc/latest/mc-$ftp_version" 
     fi
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minio-mc mc-$github_version"
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minio-mc/mc-$del_version" 
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minio-mc/mc-$del_version" 
 fi
