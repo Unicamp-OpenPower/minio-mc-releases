9b200d78fdcba4be18c4965c001d450622e658ce
juliokiyoshi <julio.k.r.matsoui@gmail.com>
2020-09-11 13:40:17 -0300

    update release.sh

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
+ROOTPATH="~/rpmbuild/RPMS/ppc64le"
+LOCALPATH="~/mc"
+REPO1="/repository/debian/ppc64el/minio-mc"
+REPO2="/repository/rpm/ppc64le/minio-mc"
+
+if [ "$github_version" != "$ftp_version" ]
+  then
+    git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
+    cd repository-scrips/
+    chmod +x empacotar-deb.sh
+    chmod +x empacotar-rpm.sh
+    sudo mv empacotar-deb.sh ..$LOCALPATH
+    sudo mv empacotar-rpm.sh ..$LOCALPATH
+    cd ..$LOCALPATH
+    sudo ./empacotar-deb.sh mc mc-$github_version $github_version " "
+    sudo ./empacotar-rpm.sh mc mc-$github_version $github_version " " "MinIO Client (mc) provides a modern alternative to UNIX commands like ls, cat, cp, mirror, diff, find etc. It supports filesystems and Amazon S3 compatible cloud storage service (AWS Signature v2 and v4)."
+fi
+
+if [[ $github_version > $ftp_version ]]
+   then
+        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio-mc-$github_version-ppc64le.deb"
+        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/minio-mc-$github_version-1.ppc64le.rpm"
+fi
