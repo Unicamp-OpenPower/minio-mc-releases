01c07df8a35610d00efe6c0ca7b37de0586b5712
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2020-09-11 16:27:01 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ then
     make
     mv mc mc-$github_version
     
-    if [[ $github_version > $ftp_version ]]
+    if [[ $github_version != $ftp_version ]]
     then
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/minio-mc/latest mc-$github_version"
         #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/minio-mc/latest/mc-$ftp_version" 
