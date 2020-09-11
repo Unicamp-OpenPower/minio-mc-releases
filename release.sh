09f021d99dfc2c8f34c2f283abbe6ce0b115afb9
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2020-09-11 17:14:33 -0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -12,14 +12,14 @@ if [ "$github_version" != "$ftp_version" ]
     cd repository-scrips/
     chmod +x empacotar-deb.sh
     chmod +x empacotar-rpm.sh
-    sudo mv empacotar-deb.sh ..$LOCALPATH
+    #sudo mv empacotar-deb.sh ..$LOCALPATH
     sudo mv empacotar-rpm.sh ..$LOCALPATH
     cd $LOCALPATH
     sudo ./empacotar-deb.sh mc mc-$github_version $github_version " "
     sudo ./empacotar-rpm.sh mc mc-$github_version $github_version " " "MinIO Client (mc) provides a modern alternative to UNIX commands like ls, cat, cp, mirror, diff, find etc. It supports filesystems and Amazon S3 compatible cloud storage service (AWS Signature v2 and v4)."
 fi
 
-if [[ $github_version > $ftp_version ]]
+if [[ $github_version != $ftp_version ]]
    then
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio-mc-$github_version-ppc64le.deb"
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/minio-mc-$github_version-1.ppc64le.rpm"
